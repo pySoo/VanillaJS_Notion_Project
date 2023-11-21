040fe43291c7dc8ea5fddd0da4f547c607b6c9e5
pySoo <soopy368@gmail.com>
2023-11-21 14:15:37 +0900

    [refactor] : MyWorkSpace 컴포넌트 내부에서 선언된 cssSelector를 사용하도록 변경

--- src/components/MyWorkSpace/MyWorkSpaceHeader.js
+++ src/components/MyWorkSpace/MyWorkSpaceHeader.js
@@ -1,10 +1,10 @@
-import { SELECTOR } from "../../constants/selector.js";
 import { TAG } from "../../constants/tag.js";
 import { PATH } from "../../routes/path.js";
 import { navigate } from "../../routes/router.js";
 import { postDocument } from "../../service/documents.js";
 import { createCustomElement } from "../../utils/dom.js";
 import { IconSVG } from "../Common/IconSVG.js";
+import { CSS_SELECTOR } from "./cssSelector.js";
 
 export default function MyWorkSpaceHeader({ $target, updateAllDocuments }) {
   const $myWorkSpaceTitle = createCustomElement({
@@ -24,7 +24,7 @@ export default function MyWorkSpaceHeader({ $target, updateAllDocuments }) {
   const $myWorkSpaceHeader = createCustomElement({
     tag: TAG.DIV,
     props: {
-      id: SELECTOR.HEADER.MY_WORKSPACE,
+      id: CSS_SELECTOR.MY_WORKSPACE.HEADER,
     },
     children: [$myWorkSpaceTitle, $myWorkSpaceAddBtn],
   });
---
+++ src/components/MyWorkSpace/cssSelector.js
@@ -0,0 +1,5 @@
+export const CSS_SELECTOR = {
+  MY_WORKSPACE: {
+    HEADER: "my-workspace-header",
+  },
+};
